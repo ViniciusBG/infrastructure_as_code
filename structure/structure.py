69bcc19327e8699051f27775a820e85aefeb0052
ViniciusBG <viniciusbastosgomes@gmail.com>
2021-06-12 19:12:37 -0300

    Deleting S#

--- structure/structure.py
+++ structure/structure.py
@@ -1,4 +1,3 @@
-
 from aws_cdk import core as cdk
 from aws_cdk import aws_s3 as s3
 
@@ -7,17 +6,6 @@ class MyStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
         name = "first-deployed-bucket"
-        s3.Bucket(
-            self,
-            id=name,
-            bucket_name=name,
-            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
-            encryption=s3.BucketEncryption.S3_MANAGED
-        )
-
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
-        name = "second-deployed-bucket"
         s3.Bucket(
             self,
             id=name,
