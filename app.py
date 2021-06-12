a57d41c05ea708f8aad6bd09d6a939f3e60aa070
ViniciusBG <viniciusbastosgomes@gmail.com>
2021-06-12 20:04:36 -0300

    TEste

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import os
 
 from aws_cdk import core
 from structure.structure import MyStack
+from structure.ec2 import EC2InstanceStack
 
 
 app = core.App()
 MyStack(app, "DeployStack")
+
 app.synth()
