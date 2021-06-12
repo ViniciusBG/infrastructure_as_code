bde24352109a49db83a7d5344c2fa2fc7c3a655c
ViniciusBG <viniciusbastosgomes@gmail.com>
2021-06-12 19:15:09 -0300

    ads third bucket

--- structure/structure.py
+++ structure/structure.py
@@ -13,3 +13,25 @@ class MyStack(cdk.Stack):
             block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
             encryption=s3.BucketEncryption.S3_MANAGED
         )
+
+    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        name = "second-deployed-bucket"
+        s3.Bucket(
+            self,
+            id=name,
+            bucket_name=name,
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+            encryption=s3.BucketEncryption.S3_MANAGED
+        )
+
+    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        name = "third-deployed-bucket"
+        s3.Bucket(
+            self,
+            id=name,
+            bucket_name=name,
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+            encryption=s3.BucketEncryption.S3_MANAGED
+        )
