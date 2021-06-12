7fc13872640cf4fb74dda76ba8523974de8f27d6
ViniciusBG <viniciusbastosgomes@gmail.com>
2021-06-12 18:07:34 -0300

    First attempt

---
+++ structure/structure.py
@@ -0,0 +1,16 @@
+
+from aws_cdk import core as cdk
+from aws_cdk import aws_s3 as s3
+
+class MyStack(cdk.Stack):
+
+    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        name = "firstDeployedBucket"
+        s3.Bucket(
+            self,
+            id=name,
+            bucket_name=name,
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+            encryption=s3.BucketEncryption.S3_MANAGED
+        )
